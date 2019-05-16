c5a9d76240f727aecb95d6af323ab1edeb7752b9
weimengsir <1711952372@qq.com>
2019-05-16 12:40:24 +0800

    first

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 REPLACE INTO `role` VALUES (1,'ADMIN');
+REPLACE INTO `role` VALUES (2,'USER');
 -- SHOW VARIABLES LIKE 'validate_password%';
 -- delete from role;
 -- insert into role values (1,"ADMIN");
