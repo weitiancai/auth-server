515b47a5682acd375940638e6865f4f3e60a8589
weimengsir <1711952372@qq.com>
2019-05-22 23:14:05 +0800

    完善

--- src/main/java/com/gpch/login/resource/HelloResource.java
+++ src/main/java/com/gpch/login/resource/HelloResource.java
@@ -1,6 +1,8 @@
 package com.gpch.login.resource;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,20 @@ public class HelloResource {
         System.out.println("here is principle interface");
         System.out.println(principal);
         System.out.println("above is principle");
+
+//        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext()
+//                .getAuthentication()
+//                .getPrincipal();
+//
+//        System.out.println(userDetails.getUsername());
+
+        Object userDetails = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (userDetails != null) {
+            System.out.println(userDetails.toString());
+        } else {
+            System.out.println("principal为空");
+        }
+
         return principal;
     }
     @GetMapping("/hello")
